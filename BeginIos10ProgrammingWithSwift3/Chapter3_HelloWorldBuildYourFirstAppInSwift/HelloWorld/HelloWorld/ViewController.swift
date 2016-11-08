d7c6aaf2034bc4523c1e9aa6b044c24500e4e324
lzh327 <lzh_327@163.com>
2016-11-08 00:32:23 -0800

    Complete the Chapter3 Build Your First App

---
+++ BeginIos10ProgrammingWithSwift3/Chapter3_HelloWorldBuildYourFirstAppInSwift/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 李臻 on 16/11/7.
+//  Copyright © 2016年 LiZhen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+
+
+}
+
