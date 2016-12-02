ecc4d5ba93f4b9443da3be22db7e10928e26acb7
Li Zhen <lzh_327@163.com>
2016-12-02 17:17:08 +0800

    Finished Chapter9 AlertViewController, AlertAction

--- BeginIos10ProgrammingWithSwift3/Chapter9_FoodPin/FoodPin/FoodPin/RestaurantTableViewController.swift
+++ BeginIos10ProgrammingWithSwift3/Chapter9_FoodPin/FoodPin/FoodPin/RestaurantTableViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class RestaurantTableViewController: UITableViewController {
-    
+
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
     
     var restaurantImages = ["cafedeadend.jpg", "homei.jpg", "teakha.jpg", "cafeloisl.jpg", "petiteoyster.jpg", "forkeerestaurant.jpg", "posatelier.jpg", "bourkestreetbakery.jpg", "haighschocolate.jpg", "palominoespresso.jpg", "upstate.jpg", "traif.jpg", "grahamavenuemeats.jpg", "wafflewolf.jpg", "fiveleaves.jpg", "cafelore.jpg", "confessional.jpg", "barrafina.jpg", "donostia.jpg", "royaloak.jpg", "caskpubkitchen.jpg"]
@@ -17,6 +17,8 @@ class RestaurantTableViewController: UITableViewController {
     var restaurantLocations = ["HongKong", "HongKong", "HongKong", "HongKong", "HongKong", "HongKong", "HongKong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
     
     var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    
+    var restaurantIsVisited = Array(repeating: false, count: 21)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,10 +65,60 @@ class RestaurantTableViewController: UITableViewController {
         
         cell.cellSet(name: restaurantNames[indexPath.row], location: restaurantLocations[indexPath.row], type: restaurantTypes[indexPath.row], thumbnail: restaurantImages[indexPath.row])
         
+        if restaurantIsVisited[indexPath.row] {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        
         cell.thumbnailImageView.layer.cornerRadius = 30
         cell.thumbnailImageView.clipsToBounds = true
 
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //Create an option menu as an action sheet
+        let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .actionSheet)
+        //Add Cancel actions to the menu
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        optionMenu.addAction(cancelAction)
+        
+        //The Call Action
+        let callActionHandler = { (action: UIAlertAction!) -> Void in
+            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertMessage, animated: true, completion: nil)
+        }
+        
+        let callAction = UIAlertAction(title: "Call " + "123-000-\(indexPath.row)", style: .default, handler: callActionHandler)
+        optionMenu.addAction(callAction)
+        
+        //The Check-in Action
+        var checkInAction: UIAlertAction
+        if self.restaurantIsVisited[indexPath.row] {
+            checkInAction = UIAlertAction(title: "Undo Check in", style: .default, handler: {
+                (action: UIAlertAction!) -> Void in
+                let cell = tableView.cellForRow(at: indexPath)
+                cell?.accessoryType = .none
+                self.restaurantIsVisited[indexPath.row] = false
+            })
+        } else {
+            checkInAction = UIAlertAction(title: "Check in", style: .default, handler: {
+                (action: UIAlertAction!) -> Void in
+                let cell = tableView.cellForRow(at: indexPath)
+                cell?.accessoryType = .checkmark
+                self.restaurantIsVisited[indexPath.row] = true
+            })
+        }
+        
+        optionMenu.addAction(checkInAction)
+        
+        //Display the menu
+        self.present(optionMenu, animated: true, completion: nil)
+        //Deselect the row
+        tableView.deselectRow(at: indexPath, animated: false)
+        
     }
  
 
