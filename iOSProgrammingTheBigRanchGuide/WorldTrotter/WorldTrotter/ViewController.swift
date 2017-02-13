491fc3a381032812b975dad0b8a0106b15129490
Li Zhen <lzh_327@163.com>
2017-02-13 16:19:52 +0800

    WorldTrotter initialized, auto layout

---
+++ iOSProgrammingTheBigRanchGuide/WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by LiZhen on 2017/2/13.
+//  Copyright © 2017年 LiZhen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
