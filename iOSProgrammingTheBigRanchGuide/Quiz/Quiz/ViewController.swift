0c651c292082d2bc4b64c5eadc00cac87debbaf0
Li Zhen <lzh_327@163.com>
2017-02-13 14:49:24 +0800

    Quiz program initialize

---
+++ iOSProgrammingTheBigRanchGuide/Quiz/Quiz/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by LiZhen on 2017/2/13.
+//  Copyright © 2017年 LiZhen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7 + 7?",
+        "What is the capital of Vermont",
+        "What is cognac made from?"
+    ]
+    
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    
+    var currentQuestionIndex: Int = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+
+
+}
+
